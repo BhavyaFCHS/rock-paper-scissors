f3a0450244fe2520d0cf3232a0e425e7a5ad970e
Bhavya Chaudhary <chaudhary.bhavya2005@gmail.com>
2024-05-06 00:18:28 -0400

    Created function to generate computer response
    The getComputerChoice() function generates random
    integers between 0 - 2, and returns either "rock",
    "paper", or "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+    // generate a random number, 0, 1, or 2
+    let num = Math.floor(Math.random() * 3);
+    switch(num) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
